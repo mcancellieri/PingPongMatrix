b2fe3dc16dd9be00e402a86a2bad2480b64e5926
Jason Carvalho <jase@jasoncarvalho.com>
2021-12-07 14:26:31 +0000

    Add function to flip alternate rows

--- calcPixelPosition.py
+++ calcPixelPosition.py
@@ -3,20 +3,36 @@ X_DIMENSION = 32
 Y_DIMENSION = 16
 
 def getpositionfromxy(x, y):
-    position = ((y-1) * X_DIMENSION) + x
+    position = (y * X_DIMENSION) + x
     return position
 
 def getxyfromposition(position):
     completerows = position // X_DIMENSION
     remainder = position % X_DIMENSION
-    y = completerows + 1
+    y = completerows
     x = remainder
     return (x,y)
 
-x = 14
-y = 1
+def flipposition(unflipped):
+    #First, get the row, 0 is first row
+    row = getxyfromposition(unflipped)[1]
+    column = getxyfromposition(unflipped)[0]
+    if row % 2 == 0:
+        # even row, no need to do anything
+        return unflipped
+    else:
+        maxValueInRow = X_DIMENSION * (row + 1)
+        return (maxValueInRow - column)
+
+x = 33
+y = 0
 print('Get position from x y: ',(x,y))
 print(getpositionfromxy(x,y))
+
+print('\n')
+
+print('Get flipped position from x y: ',(x,y))
+print(flipposition(getpositionfromxy(x,y)))
 
 print('\n')
 
